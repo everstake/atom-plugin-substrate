e65f3854ff3fd53ee661ddac3a64321a1276394d
enfipy <enfipy@gmail.com>
2019-10-24 12:46:56 +0300

    Add polkadot api provider

--- lib/components/extrinsics/modals/runExtrinsics.tsx
+++ lib/components/extrinsics/modals/runExtrinsics.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { Api } from '@polkadot/react-api';
+import { QueueConsumer } from '@polkadot/react-components/Status/Context';
+import Queue from '@polkadot/react-components/Status/Queue';
 
 import { ModalComponent } from "../../modal";
 import { DefaultButtonComponent } from "../../buttons/default";
@@ -12,12 +15,30 @@ type State = {};
 
 const DefaultState: State = {};
 
+// Todo: Get from redux store
+const wsEndpoint = "ws://165.22.206.139:9944";
+
+
 export class RunExtrinsics extends React.Component<Props, State> {
   public state: State = DefaultState;
 
   public render(): JSX.Element {
     return (
-      <ModalComponent className="add-account">
+      <ModalComponent className="run-extrinsics">
+        <React.Suspense fallback="...">
+          <Queue>
+            <QueueConsumer>
+              {({ queuePayload, queueSetTxStatus }: any): React.ReactNode => (
+                <Api
+                  queuePayload={queuePayload}
+                  queueSetTxStatus={queueSetTxStatus}
+                  url={wsEndpoint}
+                >
+                </Api>
+              )}
+            </QueueConsumer>
+          </Queue>
+        </React.Suspense>
         <div className="buttons">
           <DefaultButtonComponent
             className="cancel"
@@ -27,14 +48,14 @@ export class RunExtrinsics extends React.Component<Props, State> {
           <DefaultButtonComponent
             className="confirm"
             title="Confirm"
-            onClick={this.handleConfirm.bind(this)}
+            onClick={() => this.handleConfirm()}
           />
         </div>
       </ModalComponent>
     );
   }
 
-  private handleConfirm(_: React.MouseEvent) {
+  private handleConfirm() {
     this.props.confirmClick();
     this.props.closeModal();
     this.setState(DefaultState);
