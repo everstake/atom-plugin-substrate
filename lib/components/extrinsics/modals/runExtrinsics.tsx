c99e8478b63c3d51089720b43ea2314ae8dd0f90
enfipy <enfipy@gmail.com>
2019-10-25 02:41:22 +0300

    Add extrinsic arguments, inputs and execution

--- lib/components/extrinsics/modals/runExtrinsics.tsx
+++ lib/components/extrinsics/modals/runExtrinsics.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { ApiPromise } from "@polkadot/api";
+import { Keyring } from "@polkadot/keyring";
 
 import { IAccount } from "../../../store/modules/substrate/types";
 import { ModalComponent } from "../../modal";
 import { DefaultButtonComponent } from "../../buttons/default";
-// import { TextInputComponent } from "../../inputs/text";
+import { TextInputComponent } from "../../inputs/text";
 import { SelectInputComponent, Item } from "../../inputs/select";
 
 export interface Props {
@@ -15,44 +16,32 @@ export interface Props {
 };
 
 interface State {
-  account: {
-    selected: number;
-    items: Item[];
-  };
+  account: number;
   extrinsic: {
     selectedModule: number;
     selectedItem: number;
   };
+  pass: string;
+  args: string[];
 };
 
 const DefaultState: State = {
-  account: {
-    selected: -1,
-    items: [],
-  },
+  account: -1,
   extrinsic: {
     selectedModule: 0,
     selectedItem: 0,
   },
+  pass: "",
+  args: [],
 };
 
 export class RunExtrinsics extends React.Component<Props, State> {
   public state: State = DefaultState;
 
-  // constructor(props: Props) {
-  //   super(props);
-  //   const selectedModule = this.getDefaultItem(props.api.tx);
-  //   this.state.extrinsic.selectedModule = selectedModule;
-  //   this.state.extrinsic.selectedItem = this.getDefaultItem(props.api.tx[selectedModule]);
-  // }
-  //
-  // private getDefaultItem(val: any): string {
-  //   return Object.keys(val)[0];
-  // }
-
   public render(): JSX.Element {
     const accountItems: Item[] = this.props.accounts.map(val => ({
       label: val.meta.name,
+      value: val,
     }));
 
     return (
@@ -63,7 +52,8 @@ export class RunExtrinsics extends React.Component<Props, State> {
           items={this.getModuleItems()}
           selectedItem={this.state.extrinsic.selectedModule}
           onChange={(_: Item, idx: number) => this.setState({
-            extrinsic: { selectedModule: idx, selectedItem: this.state.extrinsic.selectedItem },
+            extrinsic: { selectedModule: idx, selectedItem: 0 },
+            args: [],
           })}
         />
         <SelectInputComponent
@@ -73,16 +63,24 @@ export class RunExtrinsics extends React.Component<Props, State> {
           selectedItem={this.state.extrinsic.selectedItem}
           onChange={(_: Item, idx: number) => this.setState({
             extrinsic: { selectedItem: idx, selectedModule: this.state.extrinsic.selectedModule },
+            args: [],
           })}
         />
+        {this.renderArguments()}
         <SelectInputComponent
           className="account"
-          title="Account key type"
+          title="Select account"
           items={accountItems}
-          selectedItem={this.state.account.selected}
-          onChange={(_: Item, idx: number) => this.setState({
-            account: { selected: idx, items: this.state.account.items },
-          })}
+          selectedItem={this.state.account}
+          onChange={(_: Item, idx: number) => this.setState({ account: idx })}
+        />
+        <TextInputComponent
+          className="pass"
+          type="password"
+          title="Account password"
+          placeholder=""
+          value={this.state.pass}
+          onChange={(val: string) => this.setState({ pass: val })}
         />
         <div className="buttons">
           <DefaultButtonComponent
@@ -100,6 +98,36 @@ export class RunExtrinsics extends React.Component<Props, State> {
     );
   }
 
+  private renderArguments(): JSX.Element {
+    const tx = this.getExtrinsic();
+    if (!tx) {
+      return <div></div>;
+    }
+    const ext = tx.value.toJSON();
+    const args = ext.args.map((val: { name: string, type: string }, idx: number) => (
+      <TextInputComponent
+        key={idx}
+        className="argument"
+        type="text"
+        title={`${val.name}: ${val.type}`}
+        placeholder=""
+        value={this.state.args[idx] || ""}
+        onChange={(val: string) => {
+          const args = this.state.args;
+          args[idx] = val;
+          this.setState({ args });
+        }}
+      />
+    ));
+    return (<div className="arguments">{args}</div>);
+  }
+
+  private getExtrinsic(): any {
+    const { selectedItem } = this.state.extrinsic;
+    const items = this.getExtrinsicItems();
+    return items[selectedItem];
+  }
+
   private getModuleItems(): Item[] {
     const mods = this.getExtrinsicModules();
     return mods.map(val => ({
@@ -108,10 +136,15 @@ export class RunExtrinsics extends React.Component<Props, State> {
   }
 
   private getExtrinsicItems(): Item[] {
-    const mod = this.getModuleItems()[this.state.extrinsic.selectedModule];
+    const { selectedModule } = this.state.extrinsic;
+    const mod = this.getModuleItems()[selectedModule];
+    if (!mod) {
+      return [];
+    }
     const [keys] = this.getExtrinsics(mod.label);
     return keys.map(val => ({
       label: val,
+      value: this.props.api.tx[mod.label][val],
     }));
   }
 
@@ -136,23 +169,55 @@ export class RunExtrinsics extends React.Component<Props, State> {
   }
 
   private handleConfirm() {
+    if (!this.props.accounts[this.state.account]) {
+      atom.notifications.addError("Account must be selected");
+      return;
+    }
+    this.exec();
     this.props.confirmClick();
     this.props.closeModal();
-    this.setState(DefaultState);
+  }
+
+  private async exec() {
+    try {
+      const con = this.props.api;
+      const keyring = new Keyring({ type: "sr25519" });
+      const account = keyring.addFromJson(this.props.accounts[this.state.account]);
+      const nonce = await con.query.system.accountNonce(account.address);
+      const extrinsic = this.getExtrinsic().value;
+      console.log(extrinsic.toJSON());
+      const unsignedTransaction = extrinsic(...this.state.args);
+      account.decodePkcs8(this.state.pass);
+
+      con.on("error", (args) => {
+        atom.notifications.addError(`Failed to execute extrinsic with args: ${args}`);
+      });
+
+      await unsignedTransaction.sign(account, { nonce: nonce as any }).send(({ events = [], status }: any) => {
+        if (status.isFinalized) {
+          const finalized = status.asFinalized.toHex();
+          console.log(`Completed at block hash: ${finalized}`);
+
+          console.log('Events:');
+          let error: string = '';
+          events.forEach(({ phase, event: { data, method, section } }: any) => {
+            const res = `\t ${phase.toString()} : ${section}.${method} ${data.toString()}`;
+            if (res.indexOf('Failed') !== -1) {
+              error += res;
+            }
+            console.log(res);
+          });
+
+          if (error !== '') {
+            // Todo: Get error
+            atom.notifications.addError(`Failed on block "${finalized}" with error: ${error}`);
+          } else {
+            atom.notifications.addSuccess(`Completed at block hash: ${finalized}`);
+          }
+        }
+      });
+    } catch (err) {
+      atom.notifications.addError(`Error on extrinsic: ${err.message}`);
+    }
   }
 }
-// <TextInputComponent
-//   className="name"
-//   type="text"
-//   title="Account name"
-//   placeholder="Alice"
-//   value={this.state.name}
-//   onChange={(val: string) => this.setState({ name: val })}
-// />
-// <SelectInputComponent
-//   className="key-type"
-//   title="Account key type"
-//   items={this.state.keyType.items}
-//   selectedItem={this.state.keyType.selected}
-//   onChange={(_: Item, idx: number) => this.selectKeyType(idx)}
-// />
